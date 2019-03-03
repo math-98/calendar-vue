a6095c00fbb904cdf71c9ef2e92a9797b4c9ed9f
Mathieu SERVIERE <mathieu@math98.fr>
2019-03-03 17:43:40 +0100

    Affichage des évènements sur le calendrier
     * Récupère les évènements depuis le serveur
     * Affiche une modale au click
     * Gestion de la suppression

--- appli/src/components/Home.vue
+++ appli/src/components/Home.vue
@@ -1,28 +1,91 @@
 <template>
   <section>
-    <FullCalendar></FullCalendar>
+    <b-alert variant="danger" show dismissible v-if="error">{{ error }}</b-alert>
+    <FullCalendar ref="calendar" :events="events" :config="calendar_config" @event-selected="eventClick"></FullCalendar>
+    <b-modal
+      id="modalEvent"
+      ref="modalEvent"
+      v-if="this.selected_event"
+      :title="this.selected_event.title"
+    >
+      Du {{ this.selected_event.start }} au {{ this.selected_event.end }}<br/>
+      <br/>
+      {{ this.selected_event.description }}<br/>
+      <br/>
+      <b-button class="col-12" variant="danger" @click="deleteSelected" v-if="!transmiting">Supprimer</b-button>
+      <div slot="modal-footer"></div>
+    </b-modal>
   </section>
 </template>
 
 <script>
 import { FullCalendar } from 'vue-full-calendar'
 import 'fullcalendar/dist/locale/fr'
+import moment from 'moment'
 
 export default {
   name: 'Home',
+  components: {
+    FullCalendar
+  },
   props: [
     'axios'
   ],
+  mounted () {
+    this.axios.request({
+      method: 'get',
+      url: '/list'
+    })
+      .then((response) => {
+        console.log(response)
+        this.transmiting = false
+        this.error = ''
+        this.events = response.data.events
+      })
+      .catch((error) => {
+        this.error = 'Une erreur est survenue lors du traitement de la requête, consultez la console pour plus d\'infos'
+        console.error(error)
+        this.transmiting = false
+      })
+  },
   data () {
     return {
       events: [],
-      config: {
-        locale: 'fr'
-      }
+      selected_event: undefined,
+      calendar_config: {
+        themeSystem: 'bootstrap4'
+      },
+      error: '',
+      transmiting: false
     }
   },
-  components: {
-    FullCalendar
+  methods: {
+    eventClick (val) {
+      this.selected_event = val
+      this.selected_event.start = moment(this.selected_event.start).format('dddd D MMMM Y HH:mm')
+      this.selected_event.end = moment(this.selected_event.end).format('dddd D MMMM Y HH:mm')
+      this.$nextTick(() => {
+        this.$refs.modalEvent.show()
+      })
+    },
+    deleteSelected () {
+      this.transmiting = true
+      this.axios.request({
+        method: 'get',
+        url: 'delete/' + this.selected_event.id
+      })
+        .then((response) => {
+          console.log(response)
+          this.$refs.calendar.$emit('remove-event', this.selected_event)
+          this.transmiting = false
+          this.$refs.modalEvent.hide()
+        })
+        .catch((error) => {
+          this.error = 'Une erreur est survenue lors du traitement de la requête, consultez la console pour plus d\'infos'
+          console.error(error)
+          this.transmiting = false
+        })
+    }
   }
 }
 </script>
